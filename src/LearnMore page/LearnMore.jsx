22e1389b680550e5c5dc9e26077021220631d08e
MousaRaza7 <mousa.raza777@gmail.com>
2024-09-05 16:56:40 +1000

    Created LearnMore.jsx

---
+++ src/LearnMore page/LearnMore.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navbar from "../components/NavBar";
+
+export default function LearnMore() {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>Learn More</h1>
+      <p>
+        Welcome to the Learn More page. Here, you can find detailed information about our project, our goals, and how we utilize data to enhance whisky discovery.
+      </p>
+      <p>
+        Our system uses advanced chemical analysis to match whisky profiles with user preferences. Dive deep into the science and technology behind our recommendations.
+      </p>
+      <p>
+        Whether you're a whisky enthusiast or just getting started, this page will help you understand the unique characteristics of different whiskies.
+      </p>
+    </div>
+  );
+}
